19f54e7b9d04a03bbb1fdeebb6a0759bd1174f0d
Administrator <Administrator@Hoss-Inspiron>
2022-06-15 14:30:45 -0400

    Initial Version.

---
+++ src/main/java/edu/cpcc/labs/demo/SpringbootGitHbrApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitHbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitHbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cpcc/labs/demo/SpringbootGitHbrApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitHbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
